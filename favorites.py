6905ff0fc63e7e175e3b076d46310c07722c4065
jkrause4s <jkrause1s@gmail.com>
2019-09-05 15:32:49 -0700

    Updated Movies

--- favorites.py
+++ favorites.py
@@ -3,8 +3,8 @@ import media
 
 #Create instances of movies to go on webpage 
  #Must use + in place of spaces
-movie_list = ["Catch+Me+If+you+can", "Inception", "Da+Vinci+Code", "The+Social+Network", 
-            "The+Return+of+the+Jedi", "This+is+Spinal+Tap", "dirty+harry","anchorman", "fellowship+of+the+ring"]
+movie_list = ["Catch+Me+If+you+can", "Raiders+of+the+Lost+Ark", "Da+Vinci+Code", "The+Social+Network", 
+            "The+Return+of+the+Jedi", "This+is+Spinal+Tap", "dirty+harry","anchorman:", "fellowship+of+the+ring"]
 
 movies = [media.Movie(x) for x in movie_list]
 
